3e8c221916104bb4f0def13ee7e755d33b31ef1b
noelia <noeliav_86@hotmail.com>
2021-05-14 10:54:58 +0200

    Añadido el autor del programa y su email

--- hola.php
+++ hola.php
@@ -1,5 +1,6 @@
 <?php
 // NOELIA PÉREZ VELARDO
+// Autor: Noelia Pérez <nperezvelardo@iessansebastian.com>
 // El nombre por defecto es Mundo
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
